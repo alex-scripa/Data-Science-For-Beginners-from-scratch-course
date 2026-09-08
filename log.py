e197ebd73d86b33f8b71aa2ffd22a3a55472793f
Alexandru Scripcenco <scripcenco.alexandru@gmail.com>
2026-09-08 22:44:31 +0300

    doc: add learning log
    add log.ipynb with a summary

---
+++ log.py
@@ -0,0 +1,11 @@
+"""Learning log for the Data Science course."""
+
+#  #Learning log.
+#
+#  #2026-09-08
+# - Сделал fork учебного репозитория.
+# - Клонировал репозиторий через GitHub Desktop.
+# - Открыл репозиторий в PyCharm.
+# - Выполнил git init.
+# - Установил pre-commit и установил Git hook.
+# - Запустил pre-commit run --all-files; проверка остановилась на несовместимости docformatter с Python 3.14.
